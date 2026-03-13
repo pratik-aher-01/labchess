896fb0ee140bb65d5af9561cf930795c586419d8
pratik <pratikaher313@gmail.com>
2026-03-13 17:54:07 +0530

    add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey:            "AIzaSyDcTgQuIoQGYefrjrLmx1URi381OfTmev4",
+  authDomain:        "labchess-4f5cb.firebaseapp.com",
+  databaseURL:       "https://labchess-4f5cb-default-rtdb.firebaseio.com",
+  projectId:         "labchess-4f5cb",
+  storageBucket:     "labchess-4f5cb.firebasestorage.app",
+  messagingSenderId: "765553562871",
+  appId:             "1:765553562871:web:f4797f92a8373cf9216ff2"
+};
+
+export default config;
